8346bbadf184da25dc998be1b7c920ad6a79cbb3
Sourjyo Deb <sourjyo@protonmail.com>
2025-02-14 03:36:53 +0530

    fixed bugs

--- main.go
+++ main.go
@@ -13,15 +13,11 @@ import (
 
 func main() {
 	// Replace with your bot token
-	password := os.Getenv("PASSWORD")
-	connStr := fmt.Sprintf("postgresql://sourjyendra:%s@ep-hidden-limit-01274492-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require", password)
+	password := os.Getenv("PASSWORD");connStr := fmt.Sprintf("postgresql://sourjyendra:%s@ep-hidden-limit-01274492-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require", password)
 	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal(err)
-		panic(err)
-	}
-	defer db.Close()
+		if err != nil { log.Fatal(err) }; defer db.Close();
 
+	State := make(map[int]string)
 	val := os.Getenv("TELEGRAM_KEY")
 	bot, err := tgbotapi.NewBotAPI(val)
 	if err != nil {
@@ -41,24 +37,33 @@ func main() {
 			continue
 		}
 
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 		//	log.Printf("%s %s", update.Message.From.FirstName, update.Message.Text)
 		//Print out the shared gps location from the message
-		if update.Message.Location != nil {
+		if update.Message.Location != nil && State[int(update.Message.Chat.ID)] == "checkin" {
 			log.Printf("Location: %f %f", update.Message.Location.Latitude, update.Message.Location.Longitude)
+			msg.ReplyToMessageID = update.Message.MessageID
+			msg.Text = "You have checked in"
+
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
 		//create commands to send location
 		if update.Message.IsCommand() {
 			switch update.Message.Command() {
-			case "start":
+			case "checkin":
+				State[int(update.Message.Chat.ID)] = "checkin"
 				msg.Text = "Please share your location"
 				msg.ReplyMarkup = tgbotapi.NewReplyKeyboard(
 					tgbotapi.NewKeyboardButtonRow(
 						tgbotapi.NewKeyboardButtonLocation("Share Location"),
 					),
 				)
+			case "checkout":
+				State[int(update.Message.Chat.ID)] = "checkout"
+				msg.Text = "You have checked out"
+
+			default:
+				msg.Text = "Unsupported command, check /help"
 			}
 
 			bot.Send(msg)
